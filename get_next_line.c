f5711cacf8048084a8fd6871058566e197d08218
ShintaroSaeki <saeki@mur4l.com>
2023-05-24 13:24:46 +0900

    ADD: create get_next_line.h

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: shisaeki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 15:06:32 by shisaeki          #+#    #+#             */
-/*   Updated: 2023/05/23 18:16:06 by shisaeki         ###   ########.fr       */
+/*   Updated: 2023/05/24 13:18:02 by shisaeki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int	open_file(char *filename)
+{
+	int	fd;
+
+	fd = open(filename, O_RDONLY);
+	return (fd);
+}
+
+char	*read_file(int fd)
+{
+	char *buf;
+
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	read(fd, buf, BUFFER_SIZE);
+	return(buf);
+}
+
+char	*get_next_line(int fd, char *buf)
+{
+	char *buf;
+	static char *save;
+
+	buf = read_file(fd)
+	ft_strlcat(save, buf);
+	if ()
+}
+
 int main(int argc, char **argv)
 {
 	char *filename;
 	int fd;
-	char *str;
+	char *buf;
 	static char *save;
+	char *line;
 
-	filename = argv[1];
-	fd = open(filename, O_RDONLY);
+	fd = open_file(argv[1]);
 
-	str = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!str)
-		return ;
-
-	read(fd, str, BUFFER_SIZE);
-	str[BUFFER_SIZE] = '\0';
-
-	printf("%s\n", str);
+	buf = read_file(fd);
+	printf("%s\n", buf);
 	return (0);
 }
